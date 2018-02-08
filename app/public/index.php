8837249bd1847e81a16e0a80ec79e38bc1533e5b
lixiao <lixiao.god@163.com>
2018-02-08 14:44:40 +0800

    composer.json

--- app/public/index.php
+++ app/public/index.php
@@ -1,11 +1,10 @@
 <?php
-use minicore\lib\MiniApp;
 
 require '../../vendor/autoload.php';
 
 $config=require dirname(dirname(__FILE__)).'/main/config/Config.php';
 $config['params']=require dirname(dirname(__FILE__)).'/main/config/params.php';
-(new MiniApp($config))->run();
+(new \minicore\lib\MiniApp($config))->run();
 
 /*$application = \minicore\lib\Mini::createObj(MiniApp::class,array('config'=>$config));
 $application->run();*/
